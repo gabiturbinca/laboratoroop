3a3b62b4019f80410d3414017646709efbd237e4
gabiturbinca <gabiturbinca2@gmail.com>
2025-03-07 19:52:27 +0200

    am reparat

--- laborator2.1/gfunctii.h
+++
@@ -1,6 +0,0 @@
-
-int cmpname(student a,student b);
-int cmpmathgrade(student a,student b);
-int cmpavggrade(student a,student b);
-int cmphisgrade(student a,student b);
-int cmpenggrade(student a,student b);
