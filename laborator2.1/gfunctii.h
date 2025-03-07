c2bc0d3d3ae10fb9b9c560fd8dd1317d2c885bcd
gabiturbinca <gabiturbinca2@gmail.com>
2025-03-07 19:48:00 +0200

    schimabt header la problema 2

---
+++ laborator2.1/gfunctii.h
@@ -0,0 +1,6 @@
+
+int cmpname(student a,student b);
+int cmpmathgrade(student a,student b);
+int cmpavggrade(student a,student b);
+int cmphisgrade(student a,student b);
+int cmpenggrade(student a,student b);
