e622f1139ab72d1bc3298bea447f0c76acdd81f9
gabiturbinca <gabiturbinca2@gmail.com>
2025-03-12 17:32:08 +0200

    am modificat si mainul

--- laborator3.1/main.cpp
+++ laborator3.1/main.cpp
@@ -9,7 +9,7 @@ int main()
 {
     printf("%d\n",Math::Add(3,5));
     printf("%f\n",Math::Add(3.3,5.6));
-    printf("%s\n",Math::Add("alex","ion"));
+    printf("%s\n",Math::Add("2004","8006"));
     printf("%d\n",Math::Add(3,5,9));
     printf("%d\n",Math::Add(5,1,2,3,4,5));
     printf("%f\n",Math::Add(3.1,5.2,3.9));
@@ -17,6 +17,8 @@ int main()
     printf("%d\n",Math::Mul(3,9,9));
     printf("%f\n",Math::Mul(3.3,9.3));
     printf("%f\n",Math::Mul(3.3,9.3,3.3));
+    printf("%s\n",Math::Add("8004","8006"));
+    printf("%s\n",Math::Add("1009004","8006"));
 
     return 0;
 }
