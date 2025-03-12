60d4bf0a498290b6a06f69c08e33cd8ff30de4e3
gabiturbinca <gabiturbinca2@gmail.com>
2025-03-12 16:37:22 +0200

    m rezolvat add-ul

--- laborator3.1/Math.cpp
+++ laborator3.1/Math.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <iostream>
 int Math::Add(int x,int y)
 {
     return x+y;
@@ -13,11 +14,35 @@ char *Math::Add(const char *x,const char *y)
         return nullptr;
     int a=strlen(x);
     int b=strlen(y);
-    char *c=(char *)malloc(sizeof(char)*(a+b));
-    for(int i=0;i<a;i++)
-        c[i]=x[i];
-    for(int i=a;i<a+b;i++)
-        c[i]=y[i-a];
+    if(a<b)
+    {
+        std::swap(a,b);
+        std::swap(x,y);
+    }
+    char *c=(char *)malloc(sizeof(char)*(std::max(a,b)+2));
+    int t=0;
+    int sz=0;
+    for(int i=0;i<b;i++)
+    {
+        t+=x[a-i-1]-'0'+y[b-i-1]-'0';
+        c[sz]=t%10+'0';
+        t/=10;
+        sz++;
+    }
+    for(int i=b;i<a;i++)
+    {
+        t+=x[a-i-1]-'0';
+        c[sz]=t%10+'0';
+        t/=10;
+        sz++;
+    }
+    if(t>0)
+        {
+            c[sz]=t+'0';
+            sz++;
+        }
+    for(int i=0;i<sz/2;i++)
+        std::swap(c[i],c[sz-i-1]);
     return c;
 }
 int Math::Add(int x,int y,int z)
